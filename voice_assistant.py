44524f73d9cc6e3d3c238b9817ebd673a85429b1
Puneet Kansal <80233425+puneet-cs@users.noreply.github.com>
2026-04-27 17:47:40 +0530

    Add files via upload

--- voice_assistant.py
+++ voice_assistant.py
@@ -37,8 +37,77 @@ def get_tts_engine():
         print(f"Failed to initialize TTS Engine: {e}")
         return None
 
+def listen_to_speech():
+    try:
+        with sr.Microphone() as source:
+            recognizer.adjust_for_ambient_noise(source, duration = 1)
+            audio = recognizer.listen(source, phrase_time_limit=10)
+        
+        text = recognizer.recognize_google(audio)
+        return text.lower()
+    except sr.UnknownValueError:
+        return "Sorry, I didn't catch you"
+    except sr.RequestError:
+        return "Speech service not available"
+    except Exception as e:
+        return f"Error: {e}"
+
+def get_ai_response(messages):
+    try:
+        response = client.chat.completions.create(
+            model = MODEL,
+            messages = messages,
+            temperature= 0.7
+        )
+        result = response.choices[0].message.content   # getting 0th reply from the LLM
+        return result.strip() if result else "Sorry, I could not generate a response"
+    except Exception as e:
+        return f"Error getting AI response: {e}"
+
 def main():
     st.title("Baby Siri Voice Assistant")
+    st.markdown("---")
+
+    # Initialize the chat
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            {"role" : "system", "content": "You are a helpful voice assistant. Reply just one line"}
+        ]
+    
+    # initialize the messages to display on screen
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # sidebar control panel
+    with st.sidebar:
+        st.header("CONTROLS")
+        tts_enabled = st.checkbox("Enable Text to Speech",value = True)
+
+        # voice gender selection
+        voice_gender = st.selectbox(
+            "Voice Gender",
+            options = ["girl", "boy"],
+            index = 0,
+            help = "Choose the Voice assistant Voice: Girl or Boy"
+            )
+        if st.button("Start Voice  Input", type ="primary",use_container_width= True):
+            with st.spinner("Listening..."):
+                user_input = listen_to_speech()
+                
+                # if input is not empty and don't have any error
+                if user_input and user_input not in ["Sorry, I didn't catch you","Speech service not available"]:
+                    st.session_state.messages.append({"role": "user", "content" : user_input})  # used to display chat on screen
+                    st.session_state.chat_history.append({"role": "user", "content" : user_input}) # use to pass to LLM
+
+                    # Get the reply from LLM
+                    with st.spinner("Thinking..."):
+                        ai_response = get_ai_response(st.session_state.chat_history)
+                        st.session_state.messages.append({"role": "assistant", "content" : ai_response})  # used to display chat on screen
+                        st.session_state.chat_history.append({"role": "assistant", "content" : ai_response}) # use to pass to LLM
+
+                    if tts_enabled:
+                        speak(ai_response, voice_gender )
+
 
 
 if __name__ == "__main__":
